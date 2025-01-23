83db84a62660c69d1adf450e4f9c4fbdf89d153b
Gavrie Grant <gavrie@usf.edu>
2025-01-23 18:17:57 -0500

    Task 1 - Store Inventory

---
+++ cc_2.js
@@ -0,0 +1,8 @@
+// Task 1 - Working with Arrays
+let products = ["Steel Pan", "Sweetbread", "Mauby", "Pelau", "Table"];
+// Add a new product
+products.push("Kennel");
+// Remove the last product
+products.pop();
+// Printing the final product list  
+console.log("Final product list:", products);
