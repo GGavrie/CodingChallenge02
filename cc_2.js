9e8675fdd27eed976124414924b0b809f6b66178
Gavrie Grant <gavrie@usf.edu>
2025-01-23 18:37:11 -0500

    Task 2 - Student Scores

--- cc_2.js
+++ cc_2.js
@@ -6,3 +6,16 @@ products.push("Kennel");
 products.pop();
 // Printing the final product list  
 console.log("Final product list:", products);
+
+
+// Task 2 - Working with Numbers
+let scores = [85, 92, 78, 95, 88];
+// Update the second score
+scores[1] = 96;
+// Calculate average
+let sum = 0;
+for (let score of scores) sum += score;
+let average = sum / scores.length;
+
+console.log("Updated scores:", scores);
+console.log("Average score:", average);
